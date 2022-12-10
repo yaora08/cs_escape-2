0273e55ce48dfc9ffd2d0303d5a8a079b71f8bee
yaora08 <hamadai0829@gmail.com>
2022-12-11 00:20:03 +0900

    main

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -20,6 +20,7 @@ class ChatsController < ApplicationController
    def create
     @chat = current_user.chats.new(chat_params)
     @chat.save
+    # redirect_to chats_show_path(@room)
    end
   
    private
